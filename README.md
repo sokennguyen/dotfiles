d901c3aa0abdc7cf705af841998d5a29d21ba11e
sokennguyen <nsongkien@gmail.com>
2024-05-24 17:29:35 +0300

    dwm and README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Kien's dotfiles
+
+dwm: `autostart.sh` start the windows at startup (2 terminals + 2 browsers) at different dwm tabs
---
+++ dwm/autostart.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#open up terminal and browser windows
+
+xdotool key Super+shift+1
+
+xdotool key Super+w
+xdotool key Super+w
+sleep 2
+
+xdotool key Super+shift+2
+xdotool key Super+shift+3
+
+
+
+xdotool key Super+2
+xdotool key Super+f
+xdotool key Super+3
+xdotool key Super+f
+
+xdotool key Super+4
+xdotool key Super+Enter
+sleep 0.2
+xdotool key Super+f
+
+
+xdotool key Super+1
+xdotool key Super+Enter
+sleep 0.2
+xdotool key Super+f
