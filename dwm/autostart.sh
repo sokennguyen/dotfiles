86718792893487dced0e557339911edba9608b5e
sokennguyen <nsongkien@gmail.com>
2024-05-28 10:53:10 +0300

    just wallpaper at startup

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -2,29 +2,5 @@
 
 #open up terminal and browser windows
 
-xdotool key Super+shift+1
-
-xdotool key Super+w
-xdotool key Super+w
-sleep 2
-
 xdotool key Super+shift+2
-xdotool key Super+shift+3
-
-
-
-xdotool key Super+2
-xdotool key Super+f
-xdotool key Super+3
-xdotool key Super+f
-
-xdotool key Super+4
-xdotool key Super+Enter
-sleep 0.2
-xdotool key Super+f
-
-
-xdotool key Super+1
-xdotool key Super+Enter
-sleep 0.2
-xdotool key Super+f
+xdotool key Super+w
